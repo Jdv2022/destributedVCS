c851ed269b6aacc5b584f9c27ea1515a7412d985
Jdv2022 <jhondennisvistal@gmail.com>
2023-05-20 20:53:32 -0700

    first commit

---
+++ models/git-command.js
@@ -0,0 +1,62 @@
+class GitCommand {
+    constructor(working_directory){
+        this.working_directory = working_directory;
+    }
+    //Command: git init 
+    init(){
+        this.staging = [];
+        this.local_repository = [];
+        return "Initialized as empty Git repository.";
+    }
+
+    //Command: git status
+    status(){    
+        const jsLocation = Object.keys(this.working_directory.new_changes)[1];
+        if(this.changes == '.'){
+            return 'You have 0 change/s.\n';
+        }
+        else{
+            return 'You have 1 change/s.\n'+jsLocation+'';
+        }
+    }
+
+    //Command: git add <filename/file directory/wildcard> 
+    add(path_file){
+        let modified_files = this.working_directory.new_changes;
+        this.changes = path_file;
+        if(modified_files[path_file]){
+            this.staging.push(modified_files[path_file]);
+            delete modified_files[path_file];
+        }
+        else if(path_file == '.' || path_file == '*'){
+            return 'Successfully added as index file/s.';
+        }
+        else{
+            return `Failed to add ${path_file}! File is not modified or missing.`;
+        }
+        return "Successfully added as index file/s.";
+    }
+
+    //Command: git commit -m "<message>"
+    commit(message){
+        if(this.staging.length > 0){
+            this.local_repository.push({ "message": message, "files": this.staging });
+            this.staging = [];
+            return "Done committing to local repository.";
+        }
+        return "Nothing to commit.";
+    }
+
+    //Command: git push
+    push(){   
+        if(this.local_repository.length > 0){
+            return "Done pushing to remote repository.";
+        } 
+        else {
+            return "Nothing to push. No committed file found.";
+        }     
+    }
+}
+
+
+module.exports = GitCommand;
